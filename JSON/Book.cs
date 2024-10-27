eec15b285ec53c78c2a784e1d37c436a00e162b1
tylika <yuliana03velikaya12@gmail.com>
2024-10-27 21:28:10 +0200

    Add project files.

---
+++ JSON/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+public class Book
+{
+
+    [JsonIgnore]
+    public int PublishingHouseId { get; set; }
+    [JsonPropertyName("Title")]
+    public string Name { get; set; }
+    public PublishingHouse PublishingHouse { get; set; }
+}
---
+++ JSON/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        string json = File.ReadAllText("C:\\Users\\Tylika\\source\\repos\\JSON\\JSON\\books.json", Encoding.UTF8);
+        List<Book> books = JsonSerializer.Deserialize<List<Book>>(json);
+
+        
+        foreach (var book in books)
+        {
+            Console.WriteLine($"Title: {book.Name}, Publisher: {book.PublishingHouse.Name}, Adress: {book.PublishingHouse.Adress}\n");
+        }
+        var options = new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            WriteIndented = true
+        };
+
+        string serializedJson = JsonSerializer.Serialize(books, options);
+        File.WriteAllText("C:\\Users\\Tylika\\source\\repos\\JSON\\JSON\\BooksUpdate.json", serializedJson);
+        Console.WriteLine("\nBooks serialized and saved to BooksUpdate.json");
+    }
+}
